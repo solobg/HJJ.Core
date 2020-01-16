f50a81ba8e5f26321b6e5b47d0942299c31ebec3
qiujiliang <501778137@qq.com>
2020-01-16 17:30:17 +0800

    reset

---
+++ YYSF.QJL.CoreApi/YYSF.QJL.CoreAPI/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YYSF.QJL.CoreAPI.Controllers
+{
+    /// <summary>
+    /// Student
+    /// </summary>
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public int Login([FromBody]string username, string password)
+        {
+            return 1;
+        }
+    }
+}
--- YYSF.QJL.Web/YYSF.QJL.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YYSF.QJL.Web.MvcApplication" Language="C#" %>
